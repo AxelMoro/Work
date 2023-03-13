f20356ea5723c5006d5c682c984dc8dff46d9e4d
AxelMoro <AxelMoro@yandex.ru>
2023-03-13 22:19:44 +1100

    Create Work.py

---
+++ Work.py
@@ -0,0 +1,12 @@
+def test_range(x, y, *check): # Укажите параметры
+    range_list = []
+    for i in chek:
+        if i >= x or i <= y:
+            range_list.append (i)
+        else:
+            print('Число за границами диапазона')
+    return range_list
+
+start = 4
+end = 12
+print(test_range(start, end, 5, 16, 32, 6, 7, 1))
